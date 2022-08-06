f18453f012252d8ae987d63c7e59cf69e281e043
Arza Rizky <panggilsajarizky@gmail.com>
2022-08-07 03:37:28 +0700

    Update services.php

--- config/services.php
+++ config/services.php
@@ -34,13 +34,13 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/login/google/callback',
+        'redirect' => 'https://www.arzarizky.space/login/google/callback',
     ],
 
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/login/github/callback',
+        'redirect' => 'https://www.arzarizky.space/login/github/callback',
     ],
 
 ];
