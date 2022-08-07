af4074d2c3beb026d7b8cef9f8d406df36d0fe67
Arza Rizky <panggilsajarizky@gmail.com>
2022-08-07 16:33:33 +0700

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -38,37 +38,43 @@ class LoginController extends Controller
     // Google callback
     public function handleGoogleCallback(\Request $request)
     {
-        try {
-            $user_google    = Socialite::driver('google')->user();
-            // dd($user_google);
-            $user           = User::where('email', $user_google->getEmail())->first();
+        $user = Socialite::driver('google')->user();
+
+        $this->_registerOrLoginUser($user);
+
+        // Return home after login
+        return redirect()->route('home');
+        // try {
+        //     $user_google    = Socialite::driver('google')->user();
+        //     // dd($user_google);
+        //     $user           = User::where('email', $user_google->getEmail())->first();
 
 
-            //jika user ada maka langsung di redirect ke halaman home
-            //jika user tidak ada maka simpan ke database
-            //$user_google menyimpan data google account seperti email, foto, dsb (cek hasil dd)
+        //     //jika user ada maka langsung di redirect ke halaman home
+        //     //jika user tidak ada maka simpan ke database
+        //     //$user_google menyimpan data google account seperti email, foto, dsb (cek hasil dd)
 
-            if($user != null){
-                Auth::login($user, true);
-                // \auth()->login($user, true);
-                return redirect()->route('home');
-            }else{
-                $create = User::Create([
-                    'provider_id'       => $user_google->getid(),
-                    'email'             => $user_google->getEmail(),
-                    'name'              => $user_google->getName(),
-                    'avatar'            => $user_google->getAvatar(),
-                    'password'          => 0,
-                ]);
+        //     if($user != null){
+        //         Auth::login($user, true);
+        //         // \auth()->login($user, true);
+        //         return redirect()->route('home');
+        //     }else{
+        //         $create = User::Create([
+        //             'provider_id'       => $user_google->getid(),
+        //             'email'             => $user_google->getEmail(),
+        //             'name'              => $user_google->getName(),
+        //             'avatar'            => $user_google->getAvatar(),
+        //             'password'          => 0,
+        //         ]);
 
-                Auth::login($create, true);
-                // auth()->login($create, true);
-                return redirect()->route('home');
-            }
+        //         Auth::login($create, true);
+        //         // auth()->login($create, true);
+        //         return redirect()->route('home');
+        //     }
 
-        } catch (\Exception $e) {
-            return redirect()->route('login');
-        }
+        // } catch (\Exception $e) {
+        //     return redirect()->route('login');
+        // }
     }
 
 
@@ -101,7 +107,11 @@ class LoginController extends Controller
         $user = Socialite::driver('github')->user();
 
         $this->_registerOrLoginUser($user);
-
+        // ser::where('username','John') -> first();
+        // $verifikasiEmail = User::where('email_verified_at', null)->first();
+        if (User::where('email_verified_at', null)) {
+            return route('verification.send')->redirect()->route('home');
+        }
         // Return home after login
         return redirect()->route('home');
     }
@@ -115,7 +125,6 @@ class LoginController extends Controller
             $user->email = $data->email;
             $user->provider_id = $data->id;
             $user->avatar = $data->avatar;
-            $user->email_verified_at = Carbon::now();
             $user->save();
         }
 
