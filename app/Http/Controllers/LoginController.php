9ec1d5d07c95bcd94f979083a8f3a88a54164638
Arza Rizky <panggilsajarizky@gmail.com>
2022-08-07 15:17:19 +0700

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,14 +1,12 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
-// use Request
-
 
 class LoginController extends Controller
 {
@@ -37,72 +35,39 @@ class LoginController extends Controller
     }
 
     // Google callback
-    // public function handleGoogleCallback(\Request $request)
-    // {
-
-    //     try {
-    //         $user_google    = Socialite::driver('google')->user();
-    //         $user           = User::where('email', $user_google->getEmail())->first();
-    //         dd($user);
-
-    //         //jika user ada maka langsung di redirect ke halaman home
-    //         //jika user tidak ada maka simpan ke database
-    //         //$user_google menyimpan data google account seperti email, foto, dsb
-
-    //         if($user != null){
-    //             Auth::login($user, true);
-    //             \auth()->login($user, true);
-    //             return redirect()->route('home');
-    //         }else{
-    //             $create = User::Create([
-    //                 'email'             => $user_google->getEmail(),
-    //                 'name'              => $user_google->getName(),
-    //                 'avatar'            => $user_google->avatar(),
-    //                 'password'          => 0,
-    //                 'email_verified_at' => now()
-    //             ]);
-
-    //             Auth::login($create, true);
-    //             auth()->login($create, true);
-    //             return redirect()->route('home');
-    //         }
-
-    //     } catch (\Exception $e) {
-    //         return redirect()->route('login');
-    //     }
-    // }
-
-    public function handleGoogleCallback()
+    public function handleGoogleCallback(\Request $request)
     {
+
         try {
             $user_google    = Socialite::driver('google')->user();
             $user           = User::where('email', $user_google->getEmail())->first();
+            dd($user);
 
             //jika user ada maka langsung di redirect ke halaman home
             //jika user tidak ada maka simpan ke database
             //$user_google menyimpan data google account seperti email, foto, dsb
 
             if($user != null){
+                // Auth::login($user, true);
                 \auth()->login($user, true);
                 return redirect()->route('home');
             }else{
                 $create = User::Create([
                     'email'             => $user_google->getEmail(),
                     'name'              => $user_google->getName(),
+                    'avatar'            => $user_google->avatar(),
                     'password'          => 0,
                     'email_verified_at' => now()
                 ]);
 
-
-                \auth()->login($create, true);
+                // Auth::login($create, true);
+                auth()->login($create, true);
                 return redirect()->route('home');
             }
 
         } catch (\Exception $e) {
             return redirect()->route('login');
         }
-
-
     }
 
 
